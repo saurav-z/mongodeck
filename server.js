df9fbdb9e08140067c8c31d8651ab530ff358f77
Saurav Phuyal <128342524+saurav-z@users.noreply.github.com>
2026-05-30 13:48:32 +0545

    Fix Limits for Json Payload

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const port = 3001;
 const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 // Connection Cache
 // Key: URI, Value: MongoClient instance
